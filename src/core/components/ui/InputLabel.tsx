04f16366593c4f272505bb8a7c87d2a435b11e37
Xanderistme <xanderitsme@gmail.com>
2025-05-31 11:59:24 -0500

    Refactor InputLabel component to utilize utility function for class names

--- src/core/components/ui/InputLabel.tsx
+++ src/core/components/ui/InputLabel.tsx
@@ -2,6 +2,7 @@ import type { JSX } from 'solid-js/jsx-runtime'
 import { Input } from '@/core/components/ui/Input'
 import { Label } from '@/core/components/ui/Label'
 import { Show } from 'solid-js'
+import { cn } from '@/core/lib/utils'
 
 interface Props extends JSX.InputHTMLAttributes<HTMLInputElement> {
   text?: string
@@ -9,8 +10,10 @@ interface Props extends JSX.InputHTMLAttributes<HTMLInputElement> {
 }
 
 export const InputLabel = ({ text, labelProps, ...props }: Props) => {
+  const { class: className, ...lbProps } = labelProps ?? {}
+
   return (
-    <Label class="w-full max-w-sm flex-col" {...labelProps}>
+    <Label class={cn('w-full max-w-sm flex-col', className)} {...lbProps}>
       <Show when={text != null}>
         <p class="text-sm self-start">{text}</p>
       </Show>
