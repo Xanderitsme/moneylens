d8da1eeaac5921cc237347bec21126c109c836d8
Xanderistme <xanderitsme@gmail.com>
2025-05-31 03:09:23 -0500

    Add font-inter to MainLayout and update color theme variables in index.css

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,7 +2,7 @@ import type { ParentComponent } from 'solid-js'
 
 export const MainLayout: ParentComponent = (props) => {
   return (
-    <div class="h-dvh bg-primary-900">
+    <div class="h-dvh bg-primary-900 font-inter">
       {props.children}
     </div>
   )
