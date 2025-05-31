65057c4249f11af0101bd85a4fa382021aa6b82d
Xanderistme <xanderitsme@gmail.com>
2025-05-31 00:47:47 -0500

    Add Header and Footer components

---
+++ src/core/components/Header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => (
+  <header class="px-4 bg-gray-950 border-b border-gray-800 shadow">
+    <div class="container mx-auto py-4">
+      <h1 class='text-lg font-semibold'>
+        Financial Management
+      </h1>
+    </div>
+  </header>
+)
