8941b83326f34fd3493eb0b6805e24a5d3af5aed
Xanderistme <xanderitsme@gmail.com>
2025-05-31 11:26:21 -0500

    Enhance DashboardLayout and GuestLayout with improved background styling

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -4,12 +4,14 @@ import type { ParentComponent } from 'solid-js'
 
 export const DashboardLayout: ParentComponent = (props) => {
   return (
-    <div class="flex h-full">
+    <div class="flex h-full bg-linear-to-br from-primary-950/10 to-primary-950/5">
       <Sidebar />
       <div class="flex flex-col grow p-2">
         {/* <Header /> */}
-        <div class="grow rounded-2xl overflow-hidden bg-primary-950">
-          {props.children}
+        <div class='grow rounded-2xl overflow-hidden bg-zinc-950'>
+          <div class="bg-primary-950/5">
+            {props.children}
+          </div>
         </div>
       </div>
     </div>
--- src/layouts/GuestLayout.tsx
+++ src/layouts/GuestLayout.tsx
@@ -1,5 +1,11 @@
 import type { ParentComponent } from 'solid-js'
 
 export const GuestLayout: ParentComponent = (props) => {
-  return <div class="h-full bg-zinc-950">{props.children}</div>
+  return (
+    <div class="h-full bg-zinc-950">
+      <div class="h-full bg-linear-to-br from-primary-950/15 to-primary-950/5">
+        {props.children}
+      </div>
+    </div>
+  )
 }
