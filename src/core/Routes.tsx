f29af6e3f5dedd9bfa234e37e1d6ebdd6135a23b
Xanderistme <xanderitsme@gmail.com>
2025-05-31 10:43:55 -0500

    Refactor Routes to wrap login and register routes in GuestLayout

--- src/core/Routes.tsx
+++ src/core/Routes.tsx
@@ -4,13 +4,16 @@ import { DashboardPage } from '@/pages/Dashboard'
 import { LoginPage } from '@/pages/auth/Login'
 import { RegisterPage } from '@/pages/auth/Register'
 import { DashboardLayout } from '@/layouts/DashboardLayout'
+import { GuestLayout } from '@/layouts/GuestLayout'
 
 export const Routes = () => (
   <Router root={MainLayout}>
     <Route path={['/', '/dashboard']} component={DashboardLayout}>
       <Route path="/" component={DashboardPage} />
     </Route>
-    <Route path="/login" component={LoginPage} />
-    <Route path="/register" component={RegisterPage} />
+    <Route component={GuestLayout}>
+      <Route path="/login" component={LoginPage} />
+      <Route path="/register" component={RegisterPage} />
+    </Route>
   </Router>
 )
