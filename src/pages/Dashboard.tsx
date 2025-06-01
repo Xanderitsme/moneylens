cce744c3bb7bc8917e8733971ef8a23029a7d362
Xanderistme <xanderitsme@gmail.com>
2025-06-01 09:20:45 -0500

    Refactor DashboardPage: remove authentication logic and sign-out button for simplified UI

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,27 +1,9 @@
-import { Button } from '@/core/components/ui/Button'
-import { useAuthContext } from '@/core/context/auth/auth.provider'
-import { useNavigate } from '@solidjs/router'
-
 export const DashboardPage = () => {
-  const { session, signOut } = useAuthContext()
-  const navigate = useNavigate()
-
   return (
     <main class="p-4 h-full">
       <h1>Dashboard page</h1>
 
-      <div class="m-4">
-        <Button
-          onClick={() => {
-            signOut()
-            navigate('/login', { replace: true })
-          }}
-        >
-          Sign out
-        </Button>
-      </div>
-
-      <p class="text-wrap">{JSON.stringify(session()?.user, undefined, 1)}</p>
+      <p class="text-wrap">This is the dashboard</p>
     </main>
   )
 }
