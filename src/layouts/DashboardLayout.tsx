d0d3fd8d52bdc8f3f4d3cc506e49c11649412cf5
Xanderistme <xanderitsme@gmail.com>
2025-06-01 09:20:57 -0500

    Refactor DashboardLayout: adjust padding for improved layout consistency

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,4 +1,3 @@
-// import { Header } from '@/core/components/Header'
 import { Sidebar } from '@/core/components/sections/Sidebar'
 import type { ParentComponent } from 'solid-js'
 
@@ -6,7 +5,7 @@ export const DashboardLayout: ParentComponent = (props) => {
   return (
     <div class="flex h-dvh w-dvw bg-linear-to-br from-primary-950/10 to-primary-950/5 overflow-hidden">
       <Sidebar />
-      <div class="h-full grow p-2 overflow-auto">
+      <div class="h-full grow p-2 pl-0 overflow-auto">
         <div class="rounded-2xl overflow-hidden bg-zinc-950 h-full">
           <div class="bg-primary-950/10 h-full">{props.children}</div>
         </div>
