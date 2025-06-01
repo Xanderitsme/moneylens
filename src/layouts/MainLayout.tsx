8d4e6c89367fc5b56e4d87e2bfbdb2508e38b3af
Xanderistme <xanderitsme@gmail.com>
2025-06-01 00:45:31 -0500

    Add overflow-hidden class to MainLayout for improved layout handling

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,7 +3,7 @@ import type { ParentComponent } from 'solid-js'
 
 export const MainLayout: ParentComponent = (props) => {
   return (
-    <div class="h-dvh w-dvw bg-zinc-900 font-inter">
+    <div class="h-dvh w-dvw bg-zinc-900 font-inter overflow-hidden">
       <AuthContextProvider>{props.children}</AuthContextProvider>
     </div>
   )
