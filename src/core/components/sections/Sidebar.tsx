9168a17efe804da92ad8e02a22cc4093994a926b
Xanderistme <xanderitsme@gmail.com>
2025-06-01 09:21:03 -0500

    Refactor Sidebar: enhance structure and styling for improved user experience

--- src/core/components/sections/Sidebar.tsx
+++ src/core/components/sections/Sidebar.tsx
@@ -1,7 +1,61 @@
+import { CoinsIcon } from '@/core/components/icons/CoinsIcon'
+import { A, type AnchorProps } from '@solidjs/router'
+import { useAuthContext } from '@/core/context/auth/auth.provider'
+import { cn } from '@/core/lib/utils'
+import { ChevronsUpDownIcon } from '../icons/ChevronsUpDownIcon'
+
+const SidebarLink = ({ children, class: className, ...props }: AnchorProps) => (
+  <A
+    class={cn(
+      'text-sm px-2 py-1.5 rounded-lg w-full inline-block',
+      'hover:bg-primary-200/10 outline-none focus-visible:ring-2 ring-primary-200/30',
+      className
+    )}
+    activeClass="bg-primary-200/10"
+    {...props}
+  >
+    {children}
+  </A>
+)
+
 export const Sidebar = () => {
+  const { session } = useAuthContext()
+
   return (
-    <aside class="px-6 py-4 w-64 h-full shrink-0">
-      <p>This is the sidebar</p>
+    <aside class="w-64 h-full shrink-0 flex flex-col gap-2">
+      <header class="p-4">
+        <SidebarLink href="/dashboard" class="block p-0" activeClass="">
+          <div class="flex flex-wrap items-center gap-3 p-2 hover:bg-primary-200/10 rounded-lg">
+            <div class="p-1 bg-primary-50 rounded-lg">
+              <CoinsIcon class="w-5 h-5 shrink-0 text-black" />
+            </div>
+            <span class="font-semibold text-primary-50 text-sm">MoneyLens</span>
+          </div>
+        </SidebarLink>
+      </header>
+      <nav class="grow p-4 space-y-2">
+        <div class="space-y-2">
+          <div class="text-xs text-zinc-400 font-semibold">Platform</div>
+          <ul class="gap-1 flex flex-col">
+            <li>
+              <SidebarLink href="/dashboard">Dashboard</SidebarLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <footer class="p-4">
+        <button class="text-sm p-2 rounded-lg w-full flex items-center gap-2 hover:bg-primary-200/10 outline-none focus-visible:ring-2 ring-primary-200/30">
+          <span class="rounded-full bg-primary-200/50 aspect-square shrink-0 size-8 flex justify-center items-center">
+            A
+          </span>
+          <div class="flex items-center overflow-hidden">
+            <span class="overflow-hidden text-ellipsis">
+              {session()?.user.email}
+            </span>
+          </div>
+          <ChevronsUpDownIcon class="size-4" />
+        </button>
+      </footer>
     </aside>
   )
 }
