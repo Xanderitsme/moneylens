f7ea5ccff396c3a659f9128e425a37f4ef7c3780
Xanderistme <xanderitsme@gmail.com>
2025-06-03 14:00:31 -0500

    Enhance DashboardPage accessibility by adding aria-label to IconButton

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -7,14 +7,14 @@ export const DashboardPage = () => {
     <>
       <header class="px-2 py-2.5 sm:px-2.5 sm:py-4 border-b border-primary-900/20">
         <div class="flex gap-1 items-center">
-          <IconButton>
+          <IconButton aria-label="toggle sidebar">
             <PanelLeftIcon class="size-4 text-primary-50 shrink-0" />
           </IconButton>
           <span class="text-sm">Dashboard</span>
         </div>
       </header>
       <main class="h-full flex flex-col">
-        <div class="p-2 sm:p-4 h-full overflow-auto scrollbar-thin">
+        <div class="p-2 sm:p-4 overflow-auto scrollbar-thin">
           <Chart2 />
         </div>
       </main>
