c8cc0dd9dfc87183d8ac7df7ea3644d9f9f14d45
Xanderitsme <136658282+Xanderitsme@users.noreply.github.com>
2025-06-09 15:33:57 -0500

    Chore: Add header to categories and transactions pages

--- src/pages/dashboard/Categories.tsx
+++ src/pages/dashboard/Categories.tsx
@@ -1,5 +1,14 @@
+import { Header } from '@/core/components/sections/Header'
+
 export const CategoriesPage = () => {
-  return <div>Categories page</div>
+  return (
+    <>
+      <Header title="Categories" />
+      <main class="grow flex flex-col p-2 sm:p-4 overflow-auto scrollbar-thin">
+        <div>Categories page</div>
+      </main>
+    </>
+  )
 }
 
 export default CategoriesPage
--- src/pages/dashboard/Transactions.tsx
+++ src/pages/dashboard/Transactions.tsx
@@ -1,5 +1,14 @@
+import { Header } from '@/core/components/sections/Header'
+
 const TransactionsPage = () => {
-  return <div>Transaction page</div>
+  return (
+    <>
+      <Header title="Transactions" />
+      <main class="grow flex flex-col p-2 sm:p-4 overflow-auto scrollbar-thin">
+        <div>Transaction page</div>
+      </main>
+    </>
+  )
 }
 
 export default TransactionsPage
