f79dbb81ff2b613dcb2618506412ca6b71f9fba0
Xanderistme <xanderitsme@gmail.com>
2025-06-10 09:50:47 -0500

    Refactor: enhance Label component structure and add support for error display

--- src/core/components/ui/Label.tsx
+++ src/core/components/ui/Label.tsx
@@ -1,18 +1,36 @@
 import { cn } from '@/core/lib/utils'
+import { Show } from 'solid-js'
 import type { JSX } from 'solid-js/jsx-runtime'
 
+interface Props extends JSX.LabelHTMLAttributes<HTMLLabelElement> {
+  text?: string
+  error?: string
+}
+
 export const Label = ({
+  children,
   class: className,
+  text,
+  error,
   ...props
-}: JSX.LabelHTMLAttributes<HTMLLabelElement>) => {
+}: Props) => {
   return (
     <label
       data-slot="label"
       class={cn(
-        'flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50',
+        'flex flex-col justify-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50',
         className
       )}
       {...props}
-    />
+    >
+      <Show when={text != null}>
+        <span class="text-zinc-300 text-sm self-start">{text}</span>
+      </Show>
+
+      {children}
+      <Show when={error != null}>
+        <span class="text-red-400 text-sm">{error}</span>
+      </Show>
+    </label>
   )
 }
