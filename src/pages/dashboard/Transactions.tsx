8b113195693f2e9b40d763383545cebf48114ce1
Xanderistme <xanderitsme@gmail.com>
2025-06-10 09:51:06 -0500

    Feat: add ButtonCreateTransaction to TransactionsPage for transaction creation

--- src/pages/dashboard/Transactions.tsx
+++ src/pages/dashboard/Transactions.tsx
@@ -1,4 +1,5 @@
 import { usePageContext } from '@/core/context/page/page.provider'
+import { ButtonCreateTransaction } from '@/dashboard/transactions/ButtonCreateTransaction'
 import { TransactionsList } from '@/dashboard/transactions/TransactionsList'
 import { onMount } from 'solid-js'
 
@@ -12,6 +13,10 @@ const TransactionsPage = () => {
   return (
     <>
       <main class="grow flex flex-col p-2 sm:p-4 overflow-auto scrollbar-thin">
+        <div class="flex justify-end gap-2">
+          <ButtonCreateTransaction />
+        </div>
+
         <section class="container mx-auto">
           <TransactionsList />
         </section>
