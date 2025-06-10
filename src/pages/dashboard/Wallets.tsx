e2739cb654c4334fb61882a9f348f67d98519c98
Xanderistme <xanderitsme@gmail.com>
2025-06-10 10:23:08 -0500

    Refactor: remove unnecessary QueryClientProvider from WalletsPage

--- src/pages/dashboard/Wallets.tsx
+++ src/pages/dashboard/Wallets.tsx
@@ -1,10 +1,7 @@
 import { usePageContext } from '@/core/context/page/page.provider'
 import { ButtonCreateWallet } from '@/dashboard/wallets/ButtonCreateWallet'
 import { WalletsList } from '@/dashboard/wallets/WalletsList'
-import { QueryClient, QueryClientProvider } from '@tanstack/solid-query'
 import { onMount } from 'solid-js'
-
-const queryClient = new QueryClient()
 
 const WalletsPage = () => {
   const { setHeaderTitle } = usePageContext()
@@ -14,19 +11,17 @@ const WalletsPage = () => {
   })
 
   const onChangeWallets = () => {
-    queryClient.invalidateQueries({ queryKey: ['wallets'] })
+    // queryClient.invalidateQueries({ queryKey: ['wallets'] })
   }
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <main class="grow flex flex-col p-2 sm:p-4 overflow-auto scrollbar-thin">
-        <div class="flex justify-end gap-2">
-          <ButtonCreateWallet onChangeWallets={onChangeWallets} />
-        </div>
+    <main class="grow flex flex-col p-2 sm:p-4 overflow-auto scrollbar-thin">
+      <div class="flex justify-end gap-2">
+        <ButtonCreateWallet onChangeWallets={onChangeWallets} />
+      </div>
 
-        <WalletsList />
-      </main>
-    </QueryClientProvider>
+      <WalletsList />
+    </main>
   )
 }
 
