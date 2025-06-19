0125d8d1b565ef6e7ff229a17bfaf6a305f48586
Xanderitsme <136658282+Xanderitsme@users.noreply.github.com>
2025-06-18 22:40:47 -0500

    Refactor: simplify TransactionsList component by removing unused state and integrating data fetching with useQuery

--- src/core/controllers/transactions.controller.ts
+++ src/core/controllers/transactions.controller.ts
@@ -1,8 +1,7 @@
 import { supabase } from '@/core/services/supabase'
 import type {
   CreateTransactionType,
-  GetTransactionsType,
-  Transaction
+  GetTransactionsType
 } from '@/types/transactions'
 
 export const createTransaction: CreateTransactionType = async ({
@@ -36,40 +35,15 @@ export const createTransaction: CreateTransactionType = async ({
   }
 }
 
-const generateTransactionsData = (count: number = 1): Transaction[] =>
-  Array.from({ length: count }).map(() => {
-    const max = 3250
-    const min = 5
-    const amount = Math.random() * max + min
-    const type = Math.random() > 0.5 ? 'expense' : 'income'
-
-    return {
-      id: '',
-      user_id: '91e341af-c6cc-496c-8d7c-f1e94c4cf5dc',
-      wallet_id: 'Efectivo',
-      category_id: 'Food',
-      amount,
-      type,
-      description:
-        "This description is really long and shouldn't be a big deal to treat with it, idk man this is just really stupid",
-      transaction_date: new Date().toISOString(),
-      created_at: '',
-      updated_at: ''
-    }
-  })
-
 export const getTransactions: GetTransactionsType = async () => {
-  // const { data, error } = await supabase
-  //   .from('transactions')
-  //   .select()
-  //   .order('transaction_date', { ascending: false })
+  const { data, error } = await supabase
+    .from('transactions')
+    .select()
+    .order('transaction_date', { ascending: false })
 
-  // if (error != null) {
-  //   return { error: { message: error.message } }
-  // }
-
-  // await new Promise((resolve) => setTimeout(resolve, 2000))
-  const data = generateTransactionsData(10)
+  if (error != null) {
+    return { error: { message: error.message } }
+  }
 
   return { data }
 }
--- src/dashboard/transactions/TransactionsList.tsx
+++ src/dashboard/transactions/TransactionsList.tsx
@@ -1,59 +1,84 @@
 import { getTransactions } from '@/core/controllers/transactions.controller'
 import { cn } from '@/core/lib/utils'
-import type { Transaction } from '@/types/transactions'
-import { createSignal, For, onMount } from 'solid-js'
+import { useQuery } from '@tanstack/solid-query'
+import { For, Match, Show, Switch } from 'solid-js'
+
+const Fallback = () => (
+  <p class="text-primary-100/60 mt-4 mx-auto">
+    You don't have any transaction yet, create one first
+  </p>
+)
 
 export const TransactionsList = () => {
-  const [transactions, setTransactions] = createSignal<Transaction[]>([])
-
-  onMount(async () => {
-    const { data, error } = await getTransactions()
-
-    if (error != null) {
-      return
-    }
-
-    setTransactions(data)
-  })
+  const query = useQuery(() => ({
+    queryKey: ['transactions'],
+    queryFn: () => getTransactions()
+  }))
 
   return (
-    <div>
-      <ul class="flex flex-col gap-4 max-w-3xl mx-auto">
-        <For each={transactions()}>
-          {(t) => (
-            <li class="flex gap-3 p-2 bg-zinc-900 rounded-lg shadow items-center border border-zinc-800 hover:border-primary-300/20">
-              <div class="aspect-square rounded-full bg-zinc-800 size-12"></div>
+    <section class="container mx-auto mt-4">
+      <Switch>
+        <Match when={query.isPending}>
+          <p class="mt-4 mx-auto">Loading...</p>
+        </Match>
+        <Match when={query.isError || query.data?.error}>
+          <p class="mt-4 mx-auto text-red-300">
+            Error: {query.error?.message ?? query.data?.error?.message}
+          </p>
+        </Match>
 
-              <div class="flex flex-col overflow-hidden">
-                <span class="text-primary-50 font-medium text-nowrap overflow-hidden text-ellipsis">
-                  {t.description}
-                </span>
+        <Match when={query.isSuccess}>
+          <Show
+            when={
+              query.data?.data?.length != null && query.data.data.length > 0
+            }
+            fallback={<Fallback />}
+          >
+            <ul class="flex flex-col gap-2 sm:gap-4 max-w-3xl mx-auto">
+              <For each={query.data?.data}>
+                {(t) => (
+                  <li class="flex gap-3 p-2 bg-zinc-900 rounded-lg shadow items-center border border-zinc-800 hover:border-primary-300/20">
+                    <div class="aspect-square rounded-full bg-zinc-800 size-12"></div>
 
-                <span class="font-medium text-zinc-400 text-sm">
-                  {new Date(t.transaction_date).toLocaleDateString('es-PE', {
-                    year: 'numeric',
-                    month: 'short',
-                    day: 'numeric'
-                  })}
-                </span>
+                    <div class="flex flex-col overflow-hidden">
+                      <span class="text-primary-50 font-medium text-nowrap overflow-hidden text-ellipsis">
+                        {t.description}
+                      </span>
 
-                <span class="text-zinc-400 text-sm">{t.wallet_id}</span>
-              </div>
+                      <span class="font-medium text-zinc-400 text-sm">
+                        {new Date(t.transaction_date).toLocaleDateString(
+                          'es-PE',
+                          {
+                            year: 'numeric',
+                            month: 'short',
+                            day: 'numeric'
+                          }
+                        )}
+                      </span>
 
-              <div class="flex items-center ml-auto px-2">
-                <span
-                  class={cn(
-                    'font-semibold text-nowrap',
-                    t.type === 'expense' ? 'text-zinc-400' : 'text-green-200'
-                  )}
-                >
-                  {t.type === 'expense' ? '-' : '+'}S/. {t.amount.toFixed(2)}
-                </span>
-              </div>
-            </li>
-          )}
-        </For>
-      </ul>
-    </div>
+                      <span class="text-zinc-400 text-sm">{t.wallet_id}</span>
+                    </div>
+
+                    <div class="flex items-center ml-auto px-2">
+                      <span
+                        class={cn(
+                          'font-semibold text-nowrap',
+                          t.type === 'expense'
+                            ? 'text-zinc-400'
+                            : 'text-green-200'
+                        )}
+                      >
+                        {t.type === 'expense' ? '-' : '+'}S/.{' '}
+                        {t.amount.toFixed(2)}
+                      </span>
+                    </div>
+                  </li>
+                )}
+              </For>
+            </ul>
+          </Show>
+        </Match>
+      </Switch>
+    </section>
   )
 }
